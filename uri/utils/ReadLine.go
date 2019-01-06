7dc97a74be7a1b91c31917f964c72a1baea37c14
Notim <paulino.joaovitor@yahoo.com.br>
2019-01-06 01:58:11 -0200

    Up

--- uri/utils/ReadLine.go
+++ uri/utils/ReadLine.go
@@ -2,13 +2,22 @@ package utils
 
 import (
 	"bufio"
-	"os"
 	"strings"
 )
 
-func ReadLine() string {
-	reader := bufio.NewReader(os.Stdin)
+// reader := bufio.NewReader(os.Stdin)
+	
+func ReadLine(reader *bufio.Reader) string {
 	input, _ := reader.ReadString('\n')
 
-	return strings.Replace(input, "\n", "", -1)
+	return ClearStr(input)
+}
+
+func ClearStr(str string) string {
+	str = strings.TrimSuffix(str, "\n")
+	str = strings.TrimSuffix(str, "\r")
+	str = strings.TrimSuffix(str, "\t")
+	str = strings.TrimSuffix(str, "\r\n")
+
+	return str
 }
