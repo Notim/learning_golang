24dcee230f3f5b4bad54ce1a15794d306f4e4fd4
Notim <paulino.joaovitor@yahoo.com.br>
2019-01-20 17:21:01 -0200

    Up

--- tries/bitwise-operators/README.md
+++ tries/bitwise-operators/README.md
@@ -272,6 +272,7 @@ a&(^b) 01000101 > 0x45 > 069
 Esse operador eh bem legal, ele empurra todos os bits para uma posiçao a direita quantas vezes forem sinalizadas,
 e onde haviam 1's viram zeros a esquerda.
 
+#### codigo 
 ```go
 func main(){
     var val byte = 128
@@ -283,7 +284,7 @@ func main(){
     }
 }
 ```
-Seguem alguns exemplos:
+#### alguns exemplos:
 ```console
 [notim@Lenovo-ideapad]$
 11000>>1 = 01100 | 24 >>1 = 12
@@ -317,7 +318,54 @@ Seguem alguns exemplos:
 >>7 00000001  0x00  001
 >>8 00000000  0x00  000
 ```
-
+### Operador shift left (>>)
+Ele empurra todos os bits para uma posiçao a esquerda quantas vezes forem sinalizadas,
+```
+   00001<<1 =    00010 //  02 <<1 = 4
+   00001<<2 =    00100 //  02 <<1 = 8
+   00001<<3 =    01000 //  02 <<1 = 16
+   00001<<4 =    10000 //  02 <<1 = 32
+10000001<<1 = 00000010 // 129 <<1 = 02 -> ele joga fora o 1 que compoe o 128 pois a variavel de exemplo s� tera espaço para 255 numeros(uint8)
+```
+### Codigo
+```go
+func main(){
+    var val byte = 0x01
+    
+    fmt.Printf("(%2d <<1 = %2d)\n", val, val<<1)
+    for i := 0; i <= 8; i++{
+        fmt.Printf("<<%d %08b  0x%02X  %03d\n", i, val, val, val)
+        val = val<<1
+    }
+}
+```
+### Alguns exemplos:
+```console
+[notim@Lenovo-ideapad]$
+(255 <<1 = 254)
+<<0 11111111  0xFF  255
+<<1 11111110  0xFE  254
+<<2 11111100  0xFC  252
+<<3 11111000  0xF8  248
+<<4 11110000  0xF0  240
+<<5 11100000  0xE0  224
+<<6 11000000  0xC0  192
+<<7 10000000  0x80  128
+<<8 00000000  0x00  000
+```
+```console
+[notim@Lenovo-ideapad]$
+( 1 <<1 =  2)
+<<0 00000001  0x01  001
+<<1 00000010  0x02  002
+<<2 00000100  0x04  004
+<<3 00001000  0x08  008
+<<4 00010000  0x10  016
+<<5 00100000  0x20  032
+<<6 01000000  0x40  064
+<<7 10000000  0x80  128
+<<8 00000000  0x00  000
+```
 ref :  
 [bit-hacking-with-go](https://medium.com/learning-the-go-programming-language/bit-hacking-with-go-e0acee258827)   
 [Bitwise-Escovando-os-bits](https://www.vivaolinux.com.br/artigo/Bitwise-Escovando-os-bits)   
--- tries/bitwise-operators/src/left-shift(<<)-operation.go
+++ tries/bitwise-operators/src/left-shift(<<)-operation.go
@@ -5,43 +5,43 @@ import "fmt"
 func main()  {
 
     /*
-        Operador shift right (>>)1
-        esse operador eh bem legal, ele empurra todos os bits para uma posiçao a direita quantas vezes forem sinalizadas,
-        e onde haviam 1's viram zeros a esquerda
+        Operador shift left (>>)
+        Ele empurra todos os bits para uma posiçao a esquerda quantas vezes forem sinalizadas,
 
         Segue alguns exemplos:
 
-        11000>>1 = 01100 // 24 >>1 = 12
-        11000>>2 = 00110 // 24 >>2 = 6
-        11000>>3 = 00011 // 24 >>3 = 3
-        11001>>1 = 01100 // 25 >>1 = 12 -> ele empura o ultimo 1 que seria o resto
+        00001<<1 =    00010 //  02 <<1 = 4
+        00001<<2 =    00100 //  02 <<1 = 8
+        00001<<3 =    01000 //  02 <<1 = 16
+        00001<<4 =    10000 //  02 <<1 = 32
+     10000001<<1 = 00000010 // 129 <<1 = 02 -> ele joga fora o 1 que compoe o 128 pois a variavel de exemplo s� tera espaço para 255 numeros(uint8)
 
-        (255 >>1 = 127)
-            11111111  0xFF  255
-        >>1 01111111  0x7F  127
-        >>2 00111111  0x3F  063
-        >>3 00011111  0x1F  031
-        >>4 00001111  0x0F  015
-        >>5 00000111  0x07  007
-        >>6 00000011  0x03  003
-        >>7 00000001  0x00  001
-        >>8 00000000  0x00  000
+        (255 <<1 = 254)
+        <<0 11111111  0xFF  255
+        <<1 11111110  0xFE  254
+        <<2 11111100  0xFC  252
+        <<3 11111000  0xF8  248
+        <<4 11110000  0xF0  240
+        <<5 11100000  0xE0  224
+        <<6 11000000  0xC0  192
+        <<7 10000000  0x80  128
+        <<8 00000000  0x00  000
 
-        (128 >>1 = 64)
-            10000000  0x80  128
-        >>1 01000000  0x40  064
-        >>2 00100000  0x20  032
-        >>3 00010000  0x10  016
-        >>4 00001000  0x08  008
-        >>5 00000100  0x04  004
-        >>6 00000010  0x02  002
-        >>7 00000001  0x00  001
-        >>8 00000000  0x00  000
+        ( 1 <<1 =  2)
+        <<0 00000001  0x01  001
+        <<1 00000010  0x02  002
+        <<2 00000100  0x04  004
+        <<3 00001000  0x08  008
+        <<4 00010000  0x10  016
+        <<5 00100000  0x20  032
+        <<6 01000000  0x40  064
+        <<7 10000000  0x80  128
+        <<8 00000000  0x00  000
     */
-    var val byte = 255
+    var val byte = 0x01
 
     fmt.Printf("(%2d <<1 = %2d)\n", val, val<<1)
-    for i :=0; i <= 8; i++{
+    for i := 0; i <= 8; i++{
         fmt.Printf("<<%d %08b  0x%02X  %03d\n", i, val, val, val)
         val = val<<1
     }
